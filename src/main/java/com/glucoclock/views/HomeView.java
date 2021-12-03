64058452c43da9e7f097976867ae57c0e641ae62
ellieYD <yokidoke607@gmail.com>
2021-12-03 17:49:13 +0000

    Home page v1 done

--- src/main/java/com/glucoclock/views/HomeView.java
+++ src/main/java/com/glucoclock/views/HomeView.java
@@ -3,6 +3,8 @@ package com.glucoclock.views;
 import com.glucoclock.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -22,9 +24,17 @@ public class HomeView extends VerticalLayout {
     private Button signUpBut;
 
     public HomeView() {
+        //Logo
+        Image logo = new Image("images/GC_logo.png","logo");
+        logo.setWidth("50%");
+        //Sign in button
         signInBut = new Button("Sign In");
         signInBut.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        signInBut.setWidth("30%");
+        //Sign up button
         signUpBut = new Button("Sign Up");
+        signUpBut.setWidth("30%");
+
 //        name = new TextField("Your name");
 //        sayHello = new Button("Say hello");
 //        sayHello.addClickListener(e -> {
@@ -32,9 +42,9 @@ public class HomeView extends VerticalLayout {
 //        });
 
         setMargin(true);
-        setHorizontalComponentAlignment(Alignment.CENTER, signInBut, signUpBut);
+        setHorizontalComponentAlignment(Alignment.CENTER, logo,signInBut, signUpBut);
 
-        add(signInBut,signUpBut);
+        add(logo,signInBut,signUpBut);
     }
 
 }
