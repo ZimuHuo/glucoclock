e9b6e80ec2e9d7ddea24c142fdebe6cbca877695
Zimu Huo <zimuhuo@outlook.com>
2021-12-04 22:36:06 +0000

    extract some codes as methods for readability

--- src/main/java/com/glucoclock/views/patient/PatientSignUp1.java
+++ src/main/java/com/glucoclock/views/patient/PatientSignUp1.java
@@ -72,25 +72,26 @@ public class PatientSignUp1 extends HorizontalLayout {
 
     private void init() {
         this.firstName = new TextField("First name");
-        firstName.setClearButtonVisible(true);
         this.lastName = new TextField("Last name");
-        lastName.setClearButtonVisible(true);
         this.emailField = new EmailField("Email Address");
-        emailFieldSetUp();
         this.password = new PasswordField("Password");
-        passwordSetUp();
         this.confirmPassword = new PasswordField("Confirm password");
+        passwordSetUp();
+        emailFieldSetUp();
+        setAlltoClearButtonVisible();
+    }
+
+    private void setAlltoClearButtonVisible() {
+        firstName.setClearButtonVisible(true);
+        lastName.setClearButtonVisible(true);
+        password.setClearButtonVisible(true);
+        confirmPassword.setClearButtonVisible(true);
+        emailField.setClearButtonVisible(true);
     }
 
 
     private void passwordSetUp() {
         password.setLabel("Password");
-        password.setClearButtonVisible(true);
-        confirmPasswordSetUp();
-    }
-
-    private void confirmPasswordSetUp() {
-        confirmPassword.setClearButtonVisible(true);
     }
 
     private void emailFieldSetUp() {
@@ -98,7 +99,6 @@ public class PatientSignUp1 extends HorizontalLayout {
         emailField.getElement().setAttribute("name", "email");
         emailField.setPlaceholder("username@example.com");
         emailField.setErrorMessage("Please enter a valid example.com email address");
-        emailField.setClearButtonVisible(true);
         emailField.setPattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
     }
 
