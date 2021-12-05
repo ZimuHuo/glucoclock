daaa0ec6214ca3edf4c391efdfaed57412239d09
Zimu Huo <zimuhuo@outlook.com>
2021-12-05 20:29:55 +0000

    added Database class for easy access and filter

---
+++ src/main/java/com/glucoclock/database/Database.java
@@ -0,0 +1,40 @@
+package com.glucoclock.database;
+
+import java.net.URISyntaxException;
+import java.sql.*;
+
+public class Database {
+
+    public Database(){
+    }
+
+
+    public static ResultSet getResultSet(String sqlStr) throws SQLException, URISyntaxException {
+
+        Connection conn = getConnection();
+        Statement s = conn.createStatement();
+        ResultSet resultSet = s.executeQuery(sqlStr);
+        return resultSet;
+    }
+
+    public static Connection getConnection() throws URISyntaxException, SQLException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        return DriverManager.getConnection(dbUrl);
+    }
+
+    public static Object getObject(String sqlStr1, String sqlStr2) throws SQLException, URISyntaxException {
+        Connection conn = getConnection();
+        Statement s = conn.createStatement();
+        ResultSet resultSet = s.executeQuery(sqlStr1);
+
+        Object data = null;
+        if (resultSet.next()) {
+            data = resultSet.getObject(sqlStr2);
+        }else{
+            return null;
+        }
+        return data;
+    }
+
+
+}
