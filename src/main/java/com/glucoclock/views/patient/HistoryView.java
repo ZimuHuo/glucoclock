2678cf5a8326ecf67f14b606aaa3bc9a8855eca3
Yifei-Jin <yj819@ic.ac.uk>
2021-12-06 18:11:35 +0000

    HistoryView 1.0

--- src/main/java/com/glucoclock/views/patient/HistoryView.java
+++ src/main/java/com/glucoclock/views/patient/HistoryView.java
@@ -4,39 +4,60 @@ package com.glucoclock.views.patient;
 import com.glucoclock.data.entity.PersonData;
 import com.glucoclock.data.entity.SamplePerson;
 import com.glucoclock.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Optional;
 
 @PageTitle("View History")
 @Route(value = "ViewHistory", layout = MainLayout.class)
 public class HistoryView extends VerticalLayout {
-    Grid<PersonData> Historylist=new Grid<>(PersonData.class,false);
-    LocalDate today=LocalDate.now();
+    private Grid<PersonData> Historylist=new Grid<>(PersonData.class,false);
+    private LocalDate today=LocalDate.now();
+    private ArrayList<PersonData> HistoryDataShown=new ArrayList<>();
+    private DatePicker SelectbyHand= new DatePicker("Select date");
+    private Button ViewData=new Button("View");
+    private HorizontalLayout SearchPanel=new HorizontalLayout();
 
     public HistoryView(){
         setSizeFull();
+        configSearch();
         configureHV();
+        setupShownData();
+        identifyclick();
 
+        //layout
+        add(
+                new H2("View History"),
+                SearchPanel,
+                Historylist);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+    }
+
+    private void configSearch() {
+        SearchPanel.add(SelectbyHand,ViewData);
+        SearchPanel.setAlignItems(Alignment.BASELINE);
+        SelectbyHand.addValueChangeListener(e-> Notification.show("the date select is: "+e.getValue().toString()));
+    }
+
+    private void identifyclick() {
         Historylist.addSelectionListener(selection-> {
             Optional <PersonData>optionalPersonData=selection.getFirstSelectedItem();
             //display date clicked
             Notification.show("Show the data at: "+optionalPersonData.get().getDatadate().toString());
         });
-
-        //layout
-        add(
-                new H2("View History"),
-                Historylist);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
 
     }
 
@@ -57,10 +78,39 @@ public class HistoryView extends VerticalLayout {
         Historylist.addColumn(PersonData::getCompleteLogBook).setHeader("Complete");
         Historylist.addColumn(PersonData::getLogBookType).setHeader("LogBook Type");
 
+
         //testing data
-        Historylist.setItems(x2,x1);
+        Historylist.setItems(x1,x2);
+
+        Historylist.setItems(HistoryDataShown);
 
         Historylist.getColumns().forEach(col->col.setAutoWidth(true));
+    }
+
+    private void setupShownData(){
+        //ArrayList<PersonData> DatabaseInput
+        //compare with database, if there is data occur at that day, add the data in the HistoryDataShown list
+        PersonData addData1=new PersonData();
+        PersonData addData2=new PersonData();
+        LocalDate date=today;
+        addData1.setDatadate(today);
+        addData1.setLogBookType("-");
+        addData1.setCompleteLogBook(false);
+        HistoryDataShown.add(addData1);
+        addData1.setDatadate(today.minusDays(1));
+        addData1.setLogBookType("-");
+        addData1.setCompleteLogBook(true);
+        HistoryDataShown.add(addData1);
+
+
+       /* for(int day=0;day<5;day++){
+            addData.setDatadate(date);
+            HistoryDataShown.add(day,addData);
+            if(day==3) addData.setCompleteLogBook(true);
+            //date=date.minusDays(1);
+
+        }*/
+
     }
 
 
