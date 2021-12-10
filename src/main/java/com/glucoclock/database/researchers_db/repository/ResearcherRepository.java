bb1f9122410cb0697115e2f6e646a19ced6246fe
G0DJ0B <zg919@ic.ac.uk>
2021-12-10 19:18:15 +0000

    updated researcher_db

---
+++ src/main/java/com/glucoclock/database/researchers_db/repository/ResearcherRepository.java
@@ -0,0 +1,20 @@
+package com.glucoclock.database.researchers_db.repository;
+
+import com.glucoclock.database.doctors_db.model.Doctor;
+import com.glucoclock.database.patients_db.model.Patient;
+import com.glucoclock.database.researchers_db.model.Researcher;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+// add update delete search
+public interface ResearcherRepository extends CrudRepository<Researcher, Long>{
+    List<Researcher> findByFirstName(String FirstName);
+    List<Researcher> findAll();
+    Researcher getResearcherById(Long id);
+    Researcher getResearcherByEmail(String email);
+    @Override
+    <S extends Researcher> S save(S s);
+
+
+}
---
+++ src/main/java/com/glucoclock/database/researchers_db/service/ResearcherService.java
@@ -0,0 +1,94 @@
+package com.glucoclock.database.researchers_db.service;
+
+import com.glucoclock.database.researchers_db.model.Researcher;
+import com.glucoclock.database.researchers_db.model.Researcher;
+import com.glucoclock.database.researchers_db.repository.ResearcherRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class ResearcherService  {
+    @Autowired
+    ResearcherRepository repository;
+
+    public String bulkcreate(){
+        // save a single Researcher
+        repository.save(new Researcher("ZImu","Huo","ZImuhuo@outlook.com","flat1","SW& $AX","12345","Male", LocalDate.of(2001,1,1),"Imperial College London"));
+        repository.save(new Researcher("ZImu2","Huo2","ZImuhuo@outlook.com2","flat2","SW& $AX2","12345","Male", LocalDate.of(2001,1,1),"Imperial College London"));
+        return "Researcher created";
+    }
+
+
+    public String search(long id){
+        String Researcher = "";
+        Researcher = repository.findById(id).toString();
+        return Researcher;
+    }
+
+
+
+    public ResearcherRepository getRepository(){
+        return repository;
+    }
+
+    public void updateResearcherLastName(long id, String LName) {
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setLastName(LName);
+        repository.save(researcher);
+    }
+
+    public void updateResearcherFirstName(long id, String FName){
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setFirstName(FName);
+        repository.save(researcher);
+    }
+    public void updateResearcherEmail(long id, String email){
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setEmail(email);
+        repository.save(researcher);
+    }
+
+    public void updateResearcherPostCode(long id, String postCode){
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setPostCode(postCode);
+        repository.save(researcher);
+    }
+
+    public void updateResearcherAddress(long id, String newHomeAddress) {
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setHomeAddress(newHomeAddress);
+        repository.save(researcher);
+    }
+
+    public void updateResearcherPhone(long id, String newPhoneNum) {
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setPhone(newPhoneNum);
+        repository.save(researcher);
+    }
+
+    public void updateResearcherGender(long id, String newGender) {
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setGender(newGender);
+        repository.save(researcher);
+    }
+
+    public void updateResearcherBirthday(long id, LocalDate newBirthday) {
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setBirthday(newBirthday);
+        repository.save(researcher);
+    }
+
+    public void updateResearcherInstitution(long id, String newInstitution) {
+        Researcher researcher = repository.getResearcherById(id);
+        researcher.setInstitution(newInstitution);
+        repository.save(researcher);
+    }
+
+}
