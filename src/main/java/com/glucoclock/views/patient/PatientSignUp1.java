3f63f8086da8fdeac85154c8395e01481f897933
Zimu Huo <zimuhuo@outlook.com>
2021-12-17 15:54:34 +0000

    finished session and cookie

--- src/main/java/com/glucoclock/views/patient/PatientSignUp1.java
+++ src/main/java/com/glucoclock/views/patient/PatientSignUp1.java
@@ -11,6 +11,8 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -77,15 +79,30 @@ public class PatientSignUp1 extends Div {
         submitButton.setIconAfterText(true);
         submitButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         submitButton.getElement().getStyle().set("margin-left", "auto");
-        submitButton.addClickListener(e ->
-                VaadinSession.getCurrent().setAttribute( "FirstName",firstName.getValue())
+        submitButton.addClickListener(e -> {
 
-//            submitButton.getUI().ifPresent(ui ->
-//                    ui.navigate(PatientSignUp2.class)
-//            )
 
-        );
 
+            if (emailField.isInvalid()) {
+                Notification notification = Notification.show("Check Emailfield");
+                notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            }else if (!password.getValue().equals(confirmPassword.getValue())){
+                Notification notification = Notification.show("Check Password");
+                notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            }else {
+
+                VaadinSession.getCurrent().setAttribute( "FirstName",firstName.getValue());
+                VaadinSession.getCurrent().setAttribute( "LastName",lastName.getValue());
+                VaadinSession.getCurrent().setAttribute( "Email",emailField.getValue());
+                VaadinSession.getCurrent().setAttribute( "Password",password.getValue());
+                submitButton.getUI().ifPresent(ui ->
+                        ui.navigate(PatientSignUp2.class)
+                );
+            }
+
+
+
+        });
     }
 
     private void formLayoutSetUp() {
@@ -119,16 +136,25 @@ public class PatientSignUp1 extends Div {
     private void lastNameSetUp() {
         lastName = new TextField("Last name");
         lastName.setClearButtonVisible(true);
+        if (VaadinSession.getCurrent().getAttribute("LastName")!= null){
+            lastName.setValue((String)VaadinSession.getCurrent().getAttribute("LastName"));
+        }
     }
 
     private void passwordSetUp() {
         password = new PasswordField("Password");
         password.setClearButtonVisible(true);
+        if (VaadinSession.getCurrent().getAttribute("Password")!= null){
+            password.setValue((String)VaadinSession.getCurrent().getAttribute("Password"));
+        }
     }
 
     private void confirmPasswordSetUp() {
         confirmPassword = new PasswordField("Confirm password");
         confirmPassword.setClearButtonVisible(true);
+        if (VaadinSession.getCurrent().getAttribute("Password")!= null){
+            confirmPassword.setValue((String)VaadinSession.getCurrent().getAttribute("Password"));
+        }
     }
 
     private void emailFieldSetUp() {
@@ -137,7 +163,10 @@ public class PatientSignUp1 extends Div {
         emailField.setPlaceholder("username@example.com");
         emailField.setErrorMessage("Please enter a valid example.com email address");
         emailField.setClearButtonVisible(true);
-        emailField.setPattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+        emailField.setPattern("(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+        if (VaadinSession.getCurrent().getAttribute("Email")!= null){
+            emailField.setValue((String)VaadinSession.getCurrent().getAttribute("Email"));
+        }
     }
 
 }
