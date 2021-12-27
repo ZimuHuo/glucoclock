bea753e40fa8da6687ece93a71a54cde37a96fee
ellieYD <yokidoke607@gmail.com>
2021-12-27 17:35:07 +0000

    prelim user repo and service

--- src/main/java/com/glucoclock/database/user_db/UserRepository.java
+++ src/main/java/com/glucoclock/database/user_db/UserRepository.java
@@ -1,4 +1,10 @@
 package com.glucoclock.database.user_db;
 
-public interface UserRepository {
+import com.glucoclock.database.patients_db.model.Patient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User getUserByEmail(String email);
+    @Override
+    <S extends User> S save(S s);
 }
--- src/main/java/com/glucoclock/database/user_db/UserService.java
+++ src/main/java/com/glucoclock/database/user_db/UserService.java
@@ -1,4 +1,15 @@
 package com.glucoclock.database.user_db;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class UserService {
+    @Autowired private BCryptPasswordEncoder encoder;
+
+    @Autowired private UserRepository userRepo;
+
+    public void save (User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        userRepo.save(user);
+    }
 }
