deaa7f625896ef2fd02a5d8acb7410584f71faaf
Zimu Huo <zimuhuo@outlook.com>
2021-12-29 19:23:18 +0000

    changed length of the verification code, just easier for testing. will change it back :D good luck with the rest !!!!

--- src/main/java/com/glucoclock/views/util/verificationCode.java
+++ src/main/java/com/glucoclock/views/util/verificationCode.java
@@ -7,10 +7,10 @@ import java.util.Random;
 public class verificationCode {
     static public String getRandomNum(){
         Random random = new Random();
-        String num = random.nextInt(99999999)+""; //nice trick for conversion in case you are wondering :D
+        String num = random.nextInt(99)+""; //nice trick for conversion in case you are wondering :D
         StringBuffer buffer = new StringBuffer();
-        for (int i =0; i<7-num.length();i++){
-            buffer.append(random.nextInt(9)); //took me a while to figure out this :D forced consistency
+        for (int i =0; i<2-num.length();i++){
+            buffer.append(random.nextInt(9)); //took me a min to figure out this bug :D forced consistency
         }
         num = buffer.toString()+num;
         return num;
