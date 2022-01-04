2acff1ada6d474e68b0fbaed918c7c7436c53cc8
G0DJ0B <zg919@ic.ac.uk>
2022-01-04 19:22:01 +0000

    Notification db

--- src/main/java/com/glucoclock/views/patient/QuestionnaireView.java
+++ src/main/java/com/glucoclock/views/patient/QuestionnaireView.java
@@ -1,5 +1,9 @@
 package com.glucoclock.views.patient;
 
+import com.glucoclock.database.doctorpatient_db.service.DoctorPatientService;
+import com.glucoclock.database.notifications_db.NotificationService;
+import com.glucoclock.database.notifications_db.Notifications;
+import com.glucoclock.database.patients_db.service.PatientService;
 import com.glucoclock.database.questionnaire_db.Questionnaire;
 import com.glucoclock.database.questionnaire_db.QuestionnaireService;
 import com.glucoclock.security.db.UserService;
@@ -16,6 +20,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -33,11 +38,18 @@ public class QuestionnaireView extends Div {
 
     private final UserService userService;
     private final QuestionnaireService questionnaireService;
+    private final NotificationService notificationService;
+    private final PatientService patientService;
+    private final DoctorPatientService doctorPatientService;
 
 
-    public QuestionnaireView(UserService userService, QuestionnaireService questionnaireService){
+    public QuestionnaireView(UserService userService, QuestionnaireService questionnaireService, NotificationService notificationService, PatientService patientService, DoctorPatientService doctorPatientService){
         this.userService = userService;
         this.questionnaireService = questionnaireService;
+        this.notificationService = notificationService;
+        this.patientService = patientService;
+        this.doctorPatientService = doctorPatientService;
+
         symptoms.setLabel("Symptoms (Select all that apply)");
         symptoms.setItems("Frequent urination",
                 "Excessive thirst",
@@ -61,6 +73,26 @@ public class QuestionnaireView extends Div {
                             otherSymptoms.getValue()
                     );
                     questionnaireService.getRepository().save(questionnaire);
+
+//                    Create and save a new notification
+                    UUID patientUID = userService.getRepository().findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()).getUid(); // Current patient UID
+                    Notifications n = new Notifications(
+                            patientService,
+                            patientUID,
+                            doctorPatientService.getRepository().getDoctorPatientByPatientuid(patientUID).getDoctoruid(), // Doctor uid
+                            "Alarm"
+                    );
+                    n.setShortMessage(symptoms.getSelectedItems().toString().substring(0,20));
+                    n.setCompleteMessage(
+                            n.getPatientFirstName() +" "+ n.getPatientLastName() +" has submitted a questionnaire.\n" +
+                                    "\n" +
+                                    "Date: " + n.getDate().toLocalDate() + "\n" +
+                                    "Time: " + n.getDate().toLocalTime() + "\n" +
+                                    "Symptoms: " + symptoms.getSelectedItems().toString()
+                    );
+                    notificationService.getRepository().save(n);
+
+
             submit.getUI().ifPresent(ui ->
                             ui.navigate(PatientStart.class) //change to feedback page later
                     );
