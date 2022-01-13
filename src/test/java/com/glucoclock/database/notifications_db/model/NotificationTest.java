faa94136cb1726a994674556e803b3363c8d960b
Zimu Huo <zimuhuo@outlook.com>
2022-01-13 14:43:16 +0000

    fixed bug

--- src/test/java/com/glucoclock/database/notifications_db/model/NotificationTest.java
+++ src/test/java/com/glucoclock/database/notifications_db/model/NotificationTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NotificationTest {
 
-    private Notification n = new Notification();
+    private Notifications n = new Notifications();
     LocalDateTime now = LocalDateTime.now();
 
 
