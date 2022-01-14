e21e1b0cdc34304378531207f6796b4468e6ac5b
Zimu Huo <zimuhuo@outlook.com>
2022-01-14 13:36:55 +0000

    password bugs

--- src/main/java/com/glucoclock/views/patient/PatientSignUp1View.java
+++ src/main/java/com/glucoclock/views/patient/PatientSignUp1View.java
@@ -188,8 +188,8 @@ public class PatientSignUp1View extends Div {
 
     private void passwordSetUp() {
         password = new PasswordField("Password");
-        password.setPattern("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
-        password.setErrorMessage("Your password must contain at least 8 characters, including 1 letter, 1 number and 1 special character");
+        password.setPattern("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+        password.setErrorMessage("Your password must contain Minimum eight characters, at least one letter and one number");
         password.setClearButtonVisible(true);
         if (VaadinSession.getCurrent().getAttribute("Password")!= null){
             password.setValue((String)VaadinSession.getCurrent().getAttribute("Password"));
